da7a05650cf0f2cb7caf2100a0799e3ed7ec7feb
CrixusZ <673659295@qq.com>
2020-10-19 22:34:44 +0800

    高阶组件

---
+++ ts-react/.js
@@ -0,0 +1,3 @@
+export {
+  // Use an empty export to please Babel's single file emit.
+}
--- ts-react/src/components/LikeBtn.tsx
+++ ts-react/src/components/LikeBtn.tsx
@@ -3,26 +3,22 @@ import useMousePosition from '../hooks/useMousePosition'
 // useState 改变函数内组件的状态，并且每次组件更新记录的状态值
 const LikeBtn: React.FC = () => {
   const [obj, setObj] = useState({like:0, on:true})
+  const [like, setLike] = useState(0)
+  const [on, setOn] = useState(true)
   const position = useMousePosition()
   useEffect(() => {
-    document.title = `点击了${obj.like}次`
-  },[obj])
+    document.title = `点击了${like}次`
+  },[like])
   return (
-    <>
+    <div>
       <p>x: {position.x}, y: {position.y}</p>
-      <button onClick={() => {setObj({
-        like:obj.like+1,
-        on:true
-      })}}>
-        {obj.like} 赞
+      <button onClick={() => {setLike(like+1)}}>
+        {like} 赞
       </button>
-      <button onClick={() => {setObj({
-        like:obj.like,
-        on:!obj.on
-      })}}>
-        {obj.on? 'ON':'OFF'} 
+      <button onClick={() => {setOn(!on)}}>
+        {on? 'ON':'OFF'} 
       </button>
-    </>
+    </div>
   )
 }
 
